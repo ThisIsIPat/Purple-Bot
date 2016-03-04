5b3954dfbb75631f79262fb083fb305bf714b0c9
IPat <mmf.ipat@gmail.com>
2016-03-04 15:00:14 +0100

    General Project Setup

---
+++ src/main/java/core/PurpleBot.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.io.IOException;
+
+/**
+ * Created by IPat on 04.03.2016.
+ */
+public class PurpleBot {
+    public static void main(String[] args) throws IOException {
+        System.out.println("This gets printed out.");
+    }
+}
